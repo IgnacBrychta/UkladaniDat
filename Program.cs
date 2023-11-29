eaccdb8375f36e4e8b9da264335e34a2e472d04e
Ignac <pomoc>
2023-11-29 08:45:37 +0100

    rozbitá práce s tabulkami, opravit

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ internal class Program
 
 	static void Main(string[] args)
 	{
+		int hash = "string".GetHashCode();
         Console.WriteLine("Hello, World!");
 		User userHonza = new User { Nickname = "přezdívka", Name = "Honza", Surname = "Novák", Email = "hu", Password = "heslo", DateOfBirth = new DateTime(1970, 11, 12) };
 		User userMatej = new User { Name = "Matej", Surname = "idk", DateOfBirth = new DateTime(1980, 12, 31), Email = "nemam@nemam.hu" };
 		string filePath = "obcane.txt";
+
+		List<User> users = new List<User>() { userHonza, userMatej};
+		User.SaveAllToTable("obcane.csv", users);
+
+		List<User> loadedUsers = User.LoadAllUsersFromTable(filePath);
+
 		userHonza.SaveToTextFile(filePath);
 		userMatej.SaveToTextFile(filePath);
 
